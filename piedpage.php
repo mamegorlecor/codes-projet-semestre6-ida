2f875384ba20a450ec4b11a8b7add92cd891d731
harounasylla1 <harouna.sylla1@unchk.edu.sn>
2026-07-03 01:27:52 +0000

    Add files via upload

---
+++ piedpage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+<title>pied de page</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="stylepiedpage.css">
+    </head>
+    <body>
+        
+        <footer class="footer">
+    <div class="contact">
+        <h1>Contact</h1>
+            <ul>
+                    <li><a href="">Adresse : Dakar Avenue Lamine Gueye</a></li>
+                    <li><a href="">Tel : 33 301 10 21</a></li>
+                    <li><a href="">Email : sunusavoirefaire@gmail.com</a></li>
+             </ul>
+    </div>
+        <div>
+            <h1>Liens rapides</h1>
+             <ul class="liensrapides">
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Exposition</a></li>
+                     <li><a href="#">S'inscrire</a></li>
+            </ul>
+    </div>
+   <p>&copy;Copyright Juin 2026</p>
+        </footer>
+    </body>
+    </html>
+    
