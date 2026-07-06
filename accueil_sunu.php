7f6908bff504de6698c82f89159064a5f9655b0c
Astou Niang <astou.niang3@unchk.edu.sn>
2026-07-06 17:44:05 +0000

    Ajout des lignes de code la page d'accueil accueil_sunu

--- accueil_sunu.php
+++ accueil_sunu.php
@@ -13,7 +13,7 @@
 </head>
 <body>
 
-    <?php include('entete.php'); ?>
+    
 
     <div class="container my-4 my-md-5 text-center">
         
@@ -75,7 +75,7 @@
 
     </div>
 
-    <?php include('piedpage.php'); ?>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
